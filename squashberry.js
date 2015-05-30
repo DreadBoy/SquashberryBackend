bf7faf6c47d40f3511c95e4a0c735fe006a0ae23
Matic Leva <vertical3life@gmail.com>
2015-05-30 17:16:33 +0200

    adding dependencies and removing some

--- squashberry.js
+++ squashberry.js
@@ -1,28 +1,39 @@
 var express = require('express'),
-    router = express.Router(),
-    database = rekuire('database');
+    router = express.Router()/*,
+    database = rekuire('database')*/;
 
 
 router
     .get('/', function (req, res) {
+        var score = {
+            name: "Something",
+            score: 0
+        };
+        res.json(score);
+        /*
         database.list(function (err, scores) {
             if(err)
                 res.status(404).send('Not found');
             else
                 res.json(scores);
         })
+        */
     })
     .post('/:name', function (req, res) {
+
         var score = {
             name: req.params.name,
             score: req.query.score
         };
+        res.json(score);
+        /*
         database.insert(score, function (err, score) {
             if(err)
                 res.status(404).send('Not found');
             else
                 res.json(score);
         });
+        */
 
     });
 
